511e92ee4f7fe2d744aa5a12869700e4094e7dbe
horvathaa <ambear9@gmail.com>
2021-07-09 11:54:45 -0400

    fix typo in groups

--- src/pages/Groups.jsx
+++ src/pages/Groups.jsx
@@ -16,13 +16,13 @@ const GroupsGuide = () => {
                     Creating a Group
                 </div>
                 <div className="adamite-description">
-                    One feature of Adamite is sharing annotations with groups of developers. By default, Adamite supports public annotations and private annotations, where public annotations are viewable by anyone and private annotations are only viewable by you, the author.
-                    You can create your own group of users to share annotations with. To create a group, select "Create New Group" from the sandwich menu.
+                        One feature of Adamite is sharing annotations with groups of developers. By default, Adamite supports public annotations and private annotations, where public annotations are viewable by anyone and private annotations are only viewable by you, the author.
+                        You can create your own group of users to share annotations with. To create a group, select "Create New Group" from the sandwich menu.
                     <div className="img-container">
                         <img src={createGroup} style={{border: "2px solid green"}} alt="create group in dropdown menu" />
                     </div>
-                    When you select "Create New Group", a modal will pop up which allows you to make a new for your group, set a description, and add users to the group. Note that the email MUST be the email your collaborator is using with Adamite.
-                    In this example, I am creating a group for Visual Studio Code Developers and, aside from myself, have invited "bam@cs.cmu.edu" as a member. Once you have filled in all of the information, click "Create".
+                        When you select "Create New Group", a modal will pop up which allows you to choose a name for your group, set a description, and add users to the group. Note that the email MUST be the email your collaborator is using with Adamite.
+                        In this example, I am creating a group for Visual Studio Code Developers and, aside from myself, have invited "bam@cs.cmu.edu" as a member. Once you have filled in all of the information, click "Create".
                     <div className="img-container">
                         <img src={createGroupCtd} style={{border: "2px solid green"}} alt="create group modal" />
                     </div>
@@ -31,21 +31,21 @@ const GroupsGuide = () => {
                 <div className="sub-sub-header">
                     Group Annotations
                 </div>
-                    Upon creating your group, you can create annotations that are shared with that group. To share an annotation, select the group name from the "Post to" dropdown.
+                        Upon creating your group, you can create annotations that are shared with that group. To share an annotation, select the group name from the "Post to" dropdown.
                     <div className="img-container">
                         <img src={selectGroupCreateAnno} style={{border: "2px solid green"}} alt="choose group in annotation dropdown menu" />
                     </div>
-                    When you've created your group annotation, you will see the group name by the author name and creation date.
+                        When you've created your group annotation, you will see the group name by the author name and creation date.
                     <div className="img-container">
                         <img src={groupAnnotation} style={{border: "2px solid green"}} alt="group annotation" />
                     </div>
-                    Other users in your group can see your annotations and interact with them, such as in this example where Brad Myers answers my question about a Visual Studio Code extension feature.
-                    Likewise, you can see your teammates' annotations.
+                        Other users in your group can see your annotations and interact with them, such as in this example where Brad Myers answers my question about a Visual Studio Code extension feature.
+                        Likewise, you can see your teammates' annotations.
                     <div className="img-container">
                         <img src={question}  alt="question and answer group annotation" />
                     </div>
-                    You can view all annotations that are in a group by selecting that group name from the groups dropdown at the top of the sidebar.
-                    In this case, I've chosen the "VS Code Extension Developers" group.
+                        You can view all annotations that are in a group by selecting that group name from the groups dropdown at the top of the sidebar.
+                        In this case, I've chosen the "VS Code Extension Developers" group.
                     <div className="img-container">
                         <img src={selectGroupFromGroup} style={{border: "2px solid green"}}  alt="group dropdown" />
                     </div>
